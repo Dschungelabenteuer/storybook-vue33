a538a273da986210862713f4f53ad5971beacd6e
Charles Gruenais <charles.gruenais@gmail.com>
2023-08-03 23:15:27 +0200

    add comments

--- src/stories/useSomething.ts
+++ src/stories/useSomething.ts
@@ -1,4 +1,18 @@
-/** Default exported composable example. */
+/**
+ * Default exported composable example.
+ *
+ * - Results in `Identifier '.default' has already been declared`
+ *
+ * - Because it compiles to (two default exports):
+ *
+ * ```
+ * export default () => {
+ *   return "something!";
+ * };
+ *
+ * export default _sfc_main;_sfc_main.__docgenInfo = {"exportName":"default","displayName":"useSomething","type":2,"props":[],"events":[],"slots":[],"exposed":[],"sourceFiles":"{dir}/src/stories/useSomething.ts"}
+ * ```
+ */
 export default () => {
   return "something!";
 };
